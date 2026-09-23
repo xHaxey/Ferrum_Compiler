edd8c6b1b04eb0dea23b5429864d787cda25b0d2
Piotr Zych <piotr.zych@student.pk.edu.pl>
2026-09-23 19:05:51 +0200

    changed operator rules to use new type system

--- Ferrum_Compiler/include/OperatorHelpers.h
+++ Ferrum_Compiler/include/OperatorHelpers.h
@@ -8,8 +8,8 @@ class OperatorRule
 {
 public:
 	OperatorRule(
-		std::vector<Type> operands, 
-		Type result, 
+		std::vector<Type*> operands, 
+		Type* result, 
 		OperatorCodegenFunc codegen, 
 		bool modifiesOperand
 	) : 
@@ -19,9 +19,9 @@ public:
 		modifiesOperand(modifiesOperand) 
 	{}
 
-	std::vector<Type> operands;
+	std::vector<Type*> operands;
 
-	Type result;
+	Type* result;
 
 	OperatorCodegenFunc codegen;
 
@@ -29,78 +29,78 @@ public:
 
 	bool Valid() const
 	{
-		return result != Type::INVALID;
+		return result != nullptr;
 	}
 };
 
-static OperatorRule addIntInt =			{ {Type::INT, Type::INT},		Type::INT,		&AddInt,		false };
-static OperatorRule addIntFloat =		{ {Type::INT, Type::FLOAT},		Type::FLOAT,	&AddFloat,		false };
-static OperatorRule addFloatInt =		{ {Type::FLOAT, Type::INT},		Type::FLOAT,	&AddFloat,		false };
-static OperatorRule addFloatFloat =		{ {Type::FLOAT, Type::FLOAT},	Type::FLOAT,	&AddFloat,		false };
+static OperatorRule addIntInt =			{ {&intType, &intType},			&intType,		&AddInt,		false };
+static OperatorRule addIntFloat =		{ {&intType, &floatType},		&floatType,		&AddFloat,		false };
+static OperatorRule addFloatInt =		{ {&floatType, &intType},		&floatType,		&AddFloat,		false };
+static OperatorRule addFloatFloat =		{ {&floatType, &floatType},		&floatType,		&AddFloat,		false };
 
-static OperatorRule subIntInt =			{ {Type::INT, Type::INT},		Type::INT,		&SubInt,		false };
-static OperatorRule subIntFloat =		{ {Type::INT, Type::FLOAT},		Type::FLOAT,	&SubFloat,		false };
-static OperatorRule subFloatInt =		{ {Type::FLOAT, Type::INT},		Type::FLOAT,	&SubFloat,		false };
-static OperatorRule subFloatFloat =		{ {Type::FLOAT, Type::FLOAT},	Type::FLOAT,	&SubFloat,		false };
+static OperatorRule subIntInt =			{ {&intType, &intType},			&intType,		&SubInt,		false };
+static OperatorRule subIntFloat =		{ {&intType, &floatType},		&floatType,		&SubFloat,		false };
+static OperatorRule subFloatInt =		{ {&floatType, &intType},		&floatType,		&SubFloat,		false };
+static OperatorRule subFloatFloat =		{ {&floatType, &floatType},		&floatType,		&SubFloat,		false };
 
-static OperatorRule mulIntInt =			{ {Type::INT, Type::INT},		Type::INT,		&MulInt,		false };
-static OperatorRule mulIntFloat =		{ {Type::INT, Type::FLOAT},		Type::FLOAT,	&MulFloat,		false };
-static OperatorRule mulFloatInt =		{ {Type::FLOAT, Type::INT},		Type::FLOAT,	&MulFloat,		false };
-static OperatorRule mulFloatFloat =		{ {Type::FLOAT, Type::FLOAT},	Type::FLOAT,	&MulFloat,		false };
+static OperatorRule mulIntInt =			{ {&intType, &intType},			&intType,		&MulInt,		false };
+static OperatorRule mulIntFloat =		{ {&intType, &floatType},		&floatType,		&MulFloat,		false };
+static OperatorRule mulFloatInt =		{ {&floatType, &intType},		&floatType,		&MulFloat,		false };
+static OperatorRule mulFloatFloat =		{ {&floatType, &floatType},		&floatType,		&MulFloat,		false };
 
-static OperatorRule divIntInt =			{ {Type::INT, Type::INT},		Type::INT,		&DivInt,		false };
-static OperatorRule divIntFloat =		{ {Type::INT, Type::FLOAT},		Type::FLOAT,	&DivFloat,		false };
-static OperatorRule divFloatInt =		{ {Type::FLOAT, Type::INT},		Type::FLOAT,	&DivFloat,		false };
-static OperatorRule divFloatFloat =		{ {Type::FLOAT, Type::FLOAT},	Type::FLOAT,	&DivFloat,		false };
+static OperatorRule divIntInt =			{ {&intType, &intType},			&intType,		&DivInt,		false };
+static OperatorRule divIntFloat =		{ {&intType, &floatType},		&floatType,		&DivFloat,		false };
+static OperatorRule divFloatInt =		{ {&floatType, &intType},		&floatType,		&DivFloat,		false };
+static OperatorRule divFloatFloat =		{ {&floatType, &floatType},		&floatType,		&DivFloat,		false };
 
-static OperatorRule modIntInt =			{ {Type::INT, Type::INT},		Type::INT,		&ModInt,		false };
-static OperatorRule modIntFloat =		{ {Type::INT, Type::FLOAT},		Type::FLOAT,	&ModFloat,		false };
-static OperatorRule modFloatInt =		{ {Type::FLOAT, Type::INT},		Type::FLOAT,	&ModFloat,		false };
-static OperatorRule modFloatFloat =		{ {Type::FLOAT, Type::FLOAT},	Type::FLOAT,	&ModFloat,		false };
+static OperatorRule modIntInt =			{ {&intType, &intType},			&intType,		&ModInt,		false };
+static OperatorRule modIntFloat =		{ {&intType, &floatType},		&floatType,		&ModFloat,		false };
+static OperatorRule modFloatInt =		{ {&floatType, &intType},		&floatType,		&ModFloat,		false };
+static OperatorRule modFloatFloat =		{ {&floatType, &floatType},		&floatType,		&ModFloat,		false };
 
-static OperatorRule grIntInt =			{ {Type::INT, Type::INT},		Type::INT,		&GreaterInt,	false };
-static OperatorRule grIntFloat =		{ {Type::INT, Type::FLOAT},		Type::FLOAT,	&GreaterFloat,	false };
-static OperatorRule grFloatInt =		{ {Type::FLOAT, Type::INT},		Type::FLOAT,	&GreaterFloat,	false };
-static OperatorRule grFloatFloat =		{ {Type::FLOAT, Type::FLOAT},	Type::FLOAT,	&GreaterFloat,	false };
+static OperatorRule grIntInt =			{ {&intType, &intType},			&intType,		&GreaterInt,	false };
+static OperatorRule grIntFloat =		{ {&intType, &floatType},		&floatType,		&GreaterFloat,	false };
+static OperatorRule grFloatInt =		{ {&floatType, &intType},		&floatType,		&GreaterFloat,	false };
+static OperatorRule grFloatFloat =		{ {&floatType, &floatType},		&floatType,		&GreaterFloat,	false };
 
-static OperatorRule greIntInt =			{ {Type::INT, Type::INT},		Type::INT,		&GreaterEqualInt,	false };
-static OperatorRule greIntFloat =		{ {Type::INT, Type::FLOAT},		Type::FLOAT,	&GreaterEqualFloat,	false };
-static OperatorRule greFloatInt =		{ {Type::FLOAT, Type::INT},		Type::FLOAT,	&GreaterEqualFloat,	false };
-static OperatorRule greFloatFloat =		{ {Type::FLOAT, Type::FLOAT},	Type::FLOAT,	&GreaterEqualFloat,	false };
+static OperatorRule greIntInt =			{ {&intType, &intType},			&intType,		&GreaterEqualInt,	false };
+static OperatorRule greIntFloat =		{ {&intType, &floatType},		&floatType,		&GreaterEqualFloat,	false };
+static OperatorRule greFloatInt =		{ {&floatType, &intType},		&floatType,		&GreaterEqualFloat,	false };
+static OperatorRule greFloatFloat =		{ {&floatType, &floatType},		&floatType,		&GreaterEqualFloat,	false };
 
-static OperatorRule lsIntInt =			{ {Type::INT, Type::INT},		Type::INT,		&LesserInt,		false };
-static OperatorRule lsIntFloat =		{ {Type::INT, Type::FLOAT},		Type::FLOAT,	&LesserFloat,	false };
-static OperatorRule lsFloatInt =		{ {Type::FLOAT, Type::INT},		Type::FLOAT,	&LesserFloat,	false };
-static OperatorRule lsFloatFloat =		{ {Type::FLOAT, Type::FLOAT},	Type::FLOAT,	&LesserFloat,	false };
+static OperatorRule lsIntInt =			{ {&intType, &intType},			&intType,		&LesserInt,		false };
+static OperatorRule lsIntFloat =		{ {&intType, &floatType},		&floatType,		&LesserFloat,	false };
+static OperatorRule lsFloatInt =		{ {&floatType, &intType},		&floatType,		&LesserFloat,	false };
+static OperatorRule lsFloatFloat =		{ {&floatType, &floatType},		&floatType,		&LesserFloat,	false };
 
-static OperatorRule lseIntInt =			{ {Type::INT, Type::INT},		Type::INT,		&LesserEqualInt,	false };
-static OperatorRule lseIntFloat =		{ {Type::INT, Type::FLOAT},		Type::FLOAT,	&LesserEqualFloat,	false };
-static OperatorRule lseFloatInt =		{ {Type::FLOAT, Type::INT},		Type::FLOAT,	&LesserEqualFloat,	false };
-static OperatorRule lseFloatFloat =		{ {Type::FLOAT, Type::FLOAT},	Type::FLOAT,	&LesserEqualFloat,	false };
+static OperatorRule lseIntInt =			{ {&intType, &intType},			&intType,		&LesserEqualInt,	false };
+static OperatorRule lseIntFloat =		{ {&intType, &floatType},		&floatType,		&LesserEqualFloat,	false };
+static OperatorRule lseFloatInt =		{ {&floatType, &intType},		&floatType,		&LesserEqualFloat,	false };
+static OperatorRule lseFloatFloat =		{ {&floatType, &floatType},		&floatType,		&LesserEqualFloat,	false };
 
-static OperatorRule eqIntInt =			{ {Type::INT, Type::INT},		Type::INT,		&EqualInt,		false };
-static OperatorRule eqIntFloat =		{ {Type::INT, Type::FLOAT},		Type::FLOAT,	&EqualFloat,	false };
-static OperatorRule eqFloatInt =		{ {Type::FLOAT, Type::INT},		Type::FLOAT,	&EqualFloat,	false };
-static OperatorRule eqFloatFloat =		{ {Type::FLOAT, Type::FLOAT},	Type::FLOAT,	&EqualFloat,	false };
+static OperatorRule eqIntInt =			{ {&intType, &intType},			&intType,		&EqualInt,		false };
+static OperatorRule eqIntFloat =		{ {&intType, &floatType},		&floatType,		&EqualFloat,	false };
+static OperatorRule eqFloatInt =		{ {&floatType, &intType},		&floatType,		&EqualFloat,	false };
+static OperatorRule eqFloatFloat =		{ {&floatType, &floatType},		&floatType,		&EqualFloat,	false };
 
-static OperatorRule neqIntInt =			{ {Type::INT, Type::INT},		Type::INT,		&NotEqualInt,	false };
-static OperatorRule neqIntFloat =		{ {Type::INT, Type::FLOAT},		Type::FLOAT,	&NotEqualFloat,	false };
-static OperatorRule neqFloatInt =		{ {Type::FLOAT, Type::INT},		Type::FLOAT,	&NotEqualFloat,	false };
-static OperatorRule neqFloatFloat =		{ {Type::FLOAT, Type::FLOAT},	Type::FLOAT,	&NotEqualFloat,	false };
+static OperatorRule neqIntInt =			{ {&intType, &intType},			&intType,		&NotEqualInt,	false };
+static OperatorRule neqIntFloat =		{ {&intType, &floatType},		&floatType,		&NotEqualFloat,	false };
+static OperatorRule neqFloatInt =		{ {&floatType, &intType},		&floatType,		&NotEqualFloat,	false };
+static OperatorRule neqFloatFloat =		{ {&floatType, &floatType},		&floatType,		&NotEqualFloat,	false };
 
-static OperatorRule andBoolBool =		{ {Type::BOOL, Type::BOOL},		Type::BOOL,		&LogicalAnd,	false };
-static OperatorRule orBoolBool =		{ {Type::BOOL, Type::BOOL},		Type::BOOL,		&LogicalOr,		false };
+static OperatorRule andBoolBool =		{ {&boolType, &boolType},		&boolType,		&LogicalAnd,	false };
+static OperatorRule orBoolBool =		{ {&boolType, &boolType},		&boolType,		&LogicalOr,		false };
 
-static OperatorRule incInt =			{ {Type::INT},					Type::INT,		&IncInt,		true };
-static OperatorRule incFloat =			{ {Type::FLOAT},				Type::FLOAT,	&IncFloat,		true };
+static OperatorRule incInt =			{ {&intType},					&intType,		&IncInt,		true };
+static OperatorRule incFloat =			{ {&floatType},					&floatType,		&IncFloat,		true };
 
-static OperatorRule decInt =			{ {Type::INT},					Type::INT,		&DecInt,		true };
-static OperatorRule decFloat =			{ {Type::FLOAT},				Type::FLOAT,	&DecFloat,		true };
+static OperatorRule decInt =			{ {&intType},					&intType,		&DecInt,		true };
+static OperatorRule decFloat =			{ {&floatType},					&floatType,		&DecFloat,		true };
 
-static OperatorRule posInt =			{ {Type::INT},					Type::INT,		&PosInt,		true };
-static OperatorRule posFloat =			{ {Type::FLOAT},				Type::FLOAT,	&PosFloat,		true };
+static OperatorRule posInt =			{ {&intType},					&intType,		&PosInt,		true };
+static OperatorRule posFloat =			{ {&floatType},					&floatType,		&PosFloat,		true };
 
-static OperatorRule negInt =			{ {Type::INT},					Type::INT,		&NegInt,		true };
-static OperatorRule negFloat =			{ {Type::FLOAT},				Type::FLOAT,	&NegFloat,		true };
+static OperatorRule negInt =			{ {&intType},					&intType,		&NegInt,		true };
+static OperatorRule negFloat =			{ {&floatType},					&floatType,		&NegFloat,		true };
 
-static OperatorRule notBool =			{ {Type::BOOL},					Type::BOOL,		&NotBool,		false };
+static OperatorRule notBool =			{ {&boolType},					&boolType,		&NotBool,		false };
